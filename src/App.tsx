afa59ae75d0f7a7798b605f9029e8baa7f220ec8
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-03 02:39:07 -0700

    Mutating Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  // return <TodoList />;
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
 
-  return <PostList />;
+  // return <PostList />;
 }
 
 export default App;
