ab10ce2594b5fdca2474121a54d32140142c1b58
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-08 23:02:59 -0700

    useContext to share states

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useReducer } from "react";
 import "./App.css";
-import LoginStatus from "./state-management/LoginStatus";
+import HomePage from "./state-management/HomePage";
+import taskReducer from "./state-management/reducers/taskReducer";
+import NavBar from "./state-management/NavBar";
+import TasksContext from "./state-management/contexts/tasksContext";
 
 function App() {
-  return <LoginStatus />;
+  const [tasks, dispatch] = useReducer(taskReducer, []);
+
+  return (
+    <TasksContext.Provider value={{ tasks, dispatch }}>
+      <NavBar />
+      <HomePage />
+    </TasksContext.Provider>
+  );
 }
 
 export default App;
