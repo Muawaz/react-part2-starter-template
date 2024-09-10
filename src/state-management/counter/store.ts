3ec5d8c225afd6d5688d2477d98a6c369154c2f2
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-10 11:34:39 +0500

    Preventing Unnecessary Renders

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 
 interface CounterStore {
     counter: number;
+    max: number;
     increament: () => void;
     reset: () => void;
 }
@@ -9,8 +10,9 @@ interface CounterStore {
 
 const useCounterStore = create<CounterStore>(set => ({
     counter: 0,
+    max: 5,
     increament: () => set(store => ({ counter: store.counter + 1 })),
-    reset: () => set(() => ({ counter: 0 }))
+    reset: () => set(() => ({ max: 10 }))
 }))
 
 export default useCounterStore
