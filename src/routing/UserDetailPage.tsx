0e1cbfa2479aa9e86f1fd07d8b8c0db387acd96b
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-12 11:01:30 +0500

    Getting data about the Current Route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,15 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  // console.log("useParams-Hook : ", params);
+  const [searchParams, setSearchParams] = useSearchParams();
+  // console.log("useSearchParams-Hook : ", searchParams);
+  // console.log("useSearchParams.toString()-Hook : ", searchParams.toString());
+  console.log("useSearchParams.get()-Hook : ", searchParams.get("name"));
+  const location = useLocation();
+  console.log("location-Hook : ", location);
+
   return <p>User</p>;
 };
 
