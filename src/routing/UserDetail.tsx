f32a6dcbbc2b290f63a55c0c8c69001a4591e2b2
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-14 11:22:52 +0500

    Error Handling - Private Routes

--- src/routing/UserDetail.tsx
+++ src/routing/UserDetail.tsx
@@ -5,8 +5,8 @@ const UserDetail = () => {
   console.log("useParams-Hook : ", params);
   const [searchParams, setSearchParams] = useSearchParams();
   // console.log("useSearchParams-Hook : ", searchParams);
-  // console.log("useSearchParams.toString()-Hook : ", searchParams.toString());
-  // console.log("useSearchParams.get()-Hook : ", searchParams.get("id"));
+  console.log("useSearchParams.toString()-Hook : ", searchParams.toString());
+  // console.log("useSearchParams.get()-Hook : ", searchParams.get("name"));
   const location = useLocation();
   console.log("location-Hook : ", location);
 
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,29 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./HomePage";
-import UserList from "./UserList";
 import ContactPage from "./ContactPage";
-import UserDetail from "./UserDetail";
+import HomePage from "./HomePage";
 import Layout from "./Layout";
+import UserDetail from "./UserDetail";
 import UserPage from "./UserPage";
+import ErrorPage from "./ErrorPage";
+import LoginPage from "./LoginPage";
+import PrivateRoutes from "./PrivateRoutes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
-
-      // { index: true, element: <HomePage /> },
-
+      { index: true, element: <HomePage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "contact", element: <ContactPage /> },
+    ],
+  },
+  {
+    element: <PrivateRoutes />,
+    children: [
       {
         path: "users",
         element: <UserPage />,
         children: [{ path: ":id", element: <UserDetail /> }],
       },
-
-      //   { path: "posts/:year/:month", element: <PostList /> },
-
-      { path: "contact", element: <ContactPage /> },
     ],
   },
 ]);
